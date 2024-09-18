8acbb84f2b82f17bb257d0352dc5b6bbac60234c
jishadaly <jishadlm10@gmail.com>
2024-09-19 00:06:42 +0530

    fix: auth handler

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,17 +1,6 @@
 // import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 // import { NextRequest, NextResponse } from "next/server";
 
-// // export const GET = (req: NextRequest , res:NextResponse) => handleAuth()(req,res);
-// export async function GET(req:NextRequest , {params}:any){
-//     const endpoint = params.kindeAuth
-//     return handleAuth(req , endpoint)
-// }  
-
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  return handleAuth()(req, NextResponse);
-}
-
-
+export async function GET(request: Request) {
+    return Response.json({ data: 'Next.js' });
+  }
--- src/app/api/polls/[pollId]/route.ts
+++ src/app/api/polls/[pollId]/route.ts
@@ -2,15 +2,13 @@ import Poll from '@/models/Poll';
 import { NextRequest, NextResponse } from 'next/server';
 
 // This is the dynamic handler for all routes like /api/[pollId]
-export async function GET(res: NextResponse, { params }: { params: { pollId: string } }) {
+export async function GET(req:NextRequest, { params }: { params: { pollId: string } }) {
     try {
         const { pollId } = params;
 
         const pollData = await Poll.findById(pollId).populate('options');
         console.log(pollData);
         return NextResponse.json(pollData,  { status: 200 });
-        
-        
     } catch (error) {
         console.log(error);
         
