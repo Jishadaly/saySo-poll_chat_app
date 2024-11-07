e3011d9c471c36ca7b7de18d34a24ddad9dca5d7
jishadaly <jishadlm10@gmail.com>
2024-11-07 15:42:25 +0530

    update toast

--- src/app/registrationCallback/page.tsx
+++ src/app/registrationCallback/page.tsx
@@ -1,61 +1,50 @@
 "use client";
 
 import React, { useEffect } from 'react';
-import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import axios from 'axios';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
 import Loading from '../Loading';
 
-export  default  function Page() {
+export default function Page() {
   const { getUser, isAuthenticated } = useKindeBrowserClient();
-  const user =  getUser();
+  const user = getUser();
   const router = useRouter();
 
   useEffect(() => {
-     
+
     if (isAuthenticated) {
       const user = getUser();
-      
+
       const storeUser = async () => {
         try {
-            const response = await axios.post('/api/user', {
+          const response = await axios.post('/api/user', {
             email: user?.email,
-            name : user?.given_name,
+            name: user?.given_name,
             picture: user?.picture,
-            
+
           });
-          
-          if (response.status === 201) {
-            toast.success(response.data);
-            router.push('/dashboard');
-          }
-          
-          toast.success(response.data);
+
+          toast.success(`${user?.given_name} logged in`);
           router.push('/dashboard');
+
+
         } catch (error) {
-          console.error('Failed to store user:', error);
+          toast.error('Registration failed, please register again')
         }
       };
 
       storeUser();
     }
   }, [isAuthenticated, getUser]);
- 
+
 
   return (
-    <div>
-      {isAuthenticated ? (
-        // 
-        <Loading/>
-      ) : (
-        // <p>Registering user...</p>
-        <Loading/>
 
-      )}
-    </div>
+    <Loading />
+
   )
 }
 
 
- 
